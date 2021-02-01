9883747d3df12a1ccdc28f329a25df10d602118d
zhaobo <zhaobo0303@163.com>
2021-02-01 09:34:07 +0800

    补充旋转数组第三种方法

--- Week_01/RotateArray.java
+++ Week_01/RotateArray.java
@@ -1,4 +1,26 @@
 class RotateArray {
+      pulic void rotate(int[] nums, int k) {
+        if (nums == null || k <= 0) return;
+        //方法3：环形旋转；将当前位置index直接移动到到 （index + k）mod n 位置，依次移动；
+        //但是当k与n存在非1公约数的话，会形成环；加入一个boolean[]记录出现过的index，如果有环的话移动到下一个元素；时间复杂度O(n),空间复杂度O(n);
+        int index = 0;
+        int hold = 0;
+        boolean[] visited = new boolean[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) hold = nums[i];
+            index = (index + k) % nums.length;
+            if (visited[index] == true) {
+                index = (index + 1) % nums.length;
+                hold = nums[index];
+                i--;
+                continue;
+            }
+            visited[index] = true;
+            int temp = nums[index];
+            nums[index] = hold;
+            hold = temp;
+        }
+    }
 
     public void rotate(int[] nums, int k) {
         if (nums == null || k <= 0) return;
